ec565590203cc7dabf0fe626c51946f73dc10652
airing <oray749616@gmail.com>
2025-01-19 00:53:59 +0800

    feat: 配置开发环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ArcoResolver} from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        // 配置 unplugin-auto-import
+        AutoImport({
+            imports: [
+                'vue',
+                'vue-router',
+                'pinia',
+            ],
+            dts: 'src/auto-imports.d.ts',
+        }),
+        // 配置 unplugin-vue-components
+        Components({
+            dts: 'src/components.d.ts',
+            resolvers: [
+                ArcoResolver(),
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/src,'
+        },
+    },
+    build: {
+        cssCodeSplit: true, // 启用 CSS 代码分割
+        minify: true,   // 启用代码压缩
+    }
 })
